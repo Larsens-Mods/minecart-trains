6ce28e25c452f99d39f9d6144101c3eccffed27b
Larsen Rektor <larsen@lrektor.de>
2025-09-01 21:46:32 +0200

    Initial Commit

---
+++ src/client/java/de/larsensmods/mctrains/MinecartTrainsClient.java
@@ -0,0 +1,10 @@
+package de.larsensmods.mctrains;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinecartTrainsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
---
+++ src/main/java/de/larsensmods/mctrains/MinecartTrains.java
@@ -0,0 +1,17 @@
+package de.larsensmods.mctrains;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MinecartTrains implements ModInitializer {
+
+	public static final String MOD_ID = "minecart-trains";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
